7b401974268dc75faa6b66154c615fb6c755caba
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-15 11:48:58 +0000

    first look at function scope

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello again dan!");
+
+const myName = "Daniel";
+
+function sayHello() {
+  console.log("Hello " + myName + "!");
+}
+
+sayHello();
+console.log(myName);
+
+function sayHelloTo() {
+  const aName = "Dan the man";
+  console.log("Hello " + aName + "!");
+}
+
+sayHelloTo();
+console.log(aName);
